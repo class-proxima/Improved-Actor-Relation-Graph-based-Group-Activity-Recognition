086bbfd9f02edf5edc809ab771b77f83f08bd536
kuangzijian <kuangzijian1@hotmail.com>
2020-10-15 01:41:01 -0600

    Added initial visualization function and to-do items

--- validate_net.py
+++ validate_net.py
@@ -136,7 +136,9 @@ def test_collective(data_loader, model, device, epoch, cfg):
 
     epoch_timer = Timer()
     with torch.no_grad():
+        i = 1
         for batch_data in data_loader:
+            ground_truth = data_loader.dataset.anns[9][i]
             # prepare batch data
             batch_data = [b.to(device=device) for b in batch_data]
             batch_size = batch_data[0].shape[0]
@@ -176,8 +178,18 @@ def test_collective(data_loader, model, device, epoch, cfg):
             activities_loss = F.cross_entropy(activities_scores, activities_in)
             activities_labels = torch.argmax(activities_scores, dim=1)  # B,
             activities_correct = torch.sum(torch.eq(activities_labels.int(), activities_in.int()).float())
-            print("Frame id: ", 1)
+
+            # Visualize the result
+            print("Frame id: ", ground_truth["frame_id"])
+            print("Bounding box position: ", ground_truth["bboxes"])
+            print("Predict actions: ", actions_labels)
             print("Predict activities: ", activities_labels)
+
+            # to-do: input Frame id, Bounding box position, Predict actions, Predict activities, 
+            # output visualized frame-like video with boxes around each person 
+            # and a "captioning" (predict actions in words and predict group activites in words)
+            # complete this visualize function
+            visualize(ground_truth["frame_id"], ground_truth["bboxes"], actions_labels, activities_labels)
 
             # Get accuracy
             actions_accuracy = actions_correct.item() / actions_scores.shape[0]
@@ -189,6 +201,7 @@ def test_collective(data_loader, model, device, epoch, cfg):
             # Total loss
             total_loss = activities_loss + cfg.actions_loss_weight * actions_loss
             loss_meter.update(total_loss.item(), batch_size)
+            i += 10
 
     test_info = {
         'time': epoch_timer.timeit(),
@@ -199,3 +212,7 @@ def test_collective(data_loader, model, device, epoch, cfg):
     }
 
     return test_info
+
+
+def visualize(param, param1, actions_labels, activities_labels):
+    pass
